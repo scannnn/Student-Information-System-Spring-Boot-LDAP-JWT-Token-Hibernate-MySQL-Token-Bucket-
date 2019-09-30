f137e6ab9afa5731749700522406f2dedc533a63
Can Sabri Çetindağ <can.cetindag@asd.com>
2019-09-30 12:49:39 +0300

    server first commit

---
+++ server/src/main/java/com/araproje/OgrenciBilgiSistemi/OgrenciBilgiSistemiApplication.java
@@ -0,0 +1,13 @@
+package com.araproje.OgrenciBilgiSistemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgrenciBilgiSistemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgrenciBilgiSistemiApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/araproje/OgrenciBilgiSistemi/OgrenciBilgiSistemiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.araproje.OgrenciBilgiSistemi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OgrenciBilgiSistemiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
