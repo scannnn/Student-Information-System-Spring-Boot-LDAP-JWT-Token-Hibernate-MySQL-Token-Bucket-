ea15b3245647ac6ac4bdf4dd50dbe88a74e06457
Can Sabri Çetindağ <can.cetindag@asd.com>
2019-11-20 13:43:34 +0300

    department controller yazıldı

--- server/src/main/java/com/araproje/OgrenciBilgiSistemi/controller/AdminRestController.java
+++ server/src/main/java/com/araproje/OgrenciBilgiSistemi/controller/AdminRestController.java
@@ -1,39 +1,123 @@
 package com.araproje.OgrenciBilgiSistemi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.araproje.OgrenciBilgiSistemi.model.Department;
-import com.araproje.OgrenciBilgiSistemi.repository.DepartmentRepository;
 import com.araproje.OgrenciBilgiSistemi.service.ClassroomService;
+import com.araproje.OgrenciBilgiSistemi.service.DepartmentService;
 
 @RestController
 @RequestMapping("/api/rest/admin")
 public class AdminRestController {
 
 	@Autowired
-	DepartmentRepository departmentRepository;
+	DepartmentService departmentService;
 	@Autowired
 	ClassroomService classroomService;
 	
-	@PutMapping("/department/add")
-	public ResponseEntity<?> deneme(@RequestBody Department department){
-		System.out.println("BURADA");
+	@PostMapping("/departments")
+	public ResponseEntity<?> add(@RequestBody Department department){
 		try {
-			departmentRepository.save(new Department(department.getDepartmentCode(), department.getTitle()));
-			System.out.println(department);
+			departmentService.create(department.getDepartmentCode(), department.getTitle());
 		}
 		catch(Exception e) {
 			return ResponseEntity
 					.status(HttpStatus.BAD_REQUEST)
-					.body("amucuk");
+					.body(e.getMessage());
 		}
-		return ResponseEntity.ok()
-	    	      .body(departmentRepository.findByDepartmentCode(department.getDepartmentCode()));
+		return ResponseEntity
+				  .status(HttpStatus.CREATED)
+	    	      .body(departmentService.get(department.getDepartmentCode()));
+	}
+	
+	
+	@DeleteMapping("/departments/{id}")
+	public ResponseEntity<?> delete(@PathVariable String id){
+		try {
+			departmentService.delete(Integer.parseInt(id));
+		}
+		catch(Exception e) {
+			return ResponseEntity
+					.status(HttpStatus.BAD_REQUEST)
+					.body(e.getMessage());
+		}
+		return ResponseEntity
+				.status(HttpStatus.ACCEPTED).body("Deleted.");
+	}
+	
+	@GetMapping("/departments")
+	public ResponseEntity<?> get(){
+		List<Department> departments;
+		try {
+			 departments = departmentService.getAll();
+		}
+		catch(Exception e) {
+			return ResponseEntity
+					.status(HttpStatus.BAD_REQUEST)
+					.body(e.getMessage());
+		}
+		return ResponseEntity
+				.status(HttpStatus.OK).body(departments);
+	}
+	
+	@GetMapping("/departments/{id}")
+	public ResponseEntity<?> getOne(@PathVariable String id){
+		Department department;
+		try {
+			 department = departmentService.get(Integer.parseInt(id));
+		}
+		catch(Exception e) {
+			return ResponseEntity
+					.status(HttpStatus.BAD_REQUEST)
+					.body(e.getMessage());
+		}
+		return ResponseEntity
+				.status(HttpStatus.OK).body(department);
+	}
+	
+	// PATCH İ TAMAMLA
+	@PatchMapping("/departments/{id}")
+	public ResponseEntity<?> update(@PathVariable String id, @RequestBody Department department){
+		try {
+			if(departmentService.isExist(Integer.parseInt(id))) {
+				departmentService.updateParticial(department, Integer.parseInt(id));
+			}
+		}
+		catch(Exception e) {
+			return ResponseEntity
+					.status(HttpStatus.BAD_REQUEST)
+					.body(e.getMessage());
+		}
+		return ResponseEntity
+				.status(HttpStatus.ACCEPTED).body(department);
+	}
+	
+	@PutMapping("/departments/{id}")
+	public ResponseEntity<?> updateAll(@PathVariable String id, @RequestBody Department department){
+		try {
+			if(departmentService.isExist(Integer.parseInt(id))) {
+				departmentService.update(department, Integer.parseInt(id));
+			}
+		}
+		catch(Exception e) {
+			return ResponseEntity
+					.status(HttpStatus.BAD_REQUEST)
+					.body(e.getMessage());
+		}
+		return ResponseEntity
+				.status(HttpStatus.ACCEPTED).body(department);
 	}
 }
--- server/src/main/java/com/araproje/OgrenciBilgiSistemi/service/DepartmentService.java
+++ server/src/main/java/com/araproje/OgrenciBilgiSistemi/service/DepartmentService.java
@@ -1,7 +1,12 @@
 package com.araproje.OgrenciBilgiSistemi.service;
 
+import java.beans.FeatureDescriptor;
 import java.util.List;
+import java.util.stream.Stream;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +23,26 @@ public class DepartmentService {
 		departmentRepository.save(new Department(departmentCode, title));
 	}
 	
-	public void update(Department department) {
+	public void update(Department department, Integer id) {
+		department.setId(id);
 		departmentRepository.save(department);
 	}
-	// OPTIONAL GİRDİLER YOLLANABİLİR Mİ METHODA YA DA UPDATE İÇİN HEPSİ DOLU AL 
+	
+	public void updateParticial(Department department, Integer id) {
+		Department persistedDepartment = departmentRepository.findById(id).get();
+		   String[] ignoredProperties = getNullPropertyNames(department);
+		   BeanUtils.copyProperties(department, persistedDepartment, ignoredProperties);
+		   departmentRepository.save(department);
+	}
+
+	public void delete(Integer id) {
+		departmentRepository.deleteById(id);
+	}
+	
+	public Department get(Integer id) {
+		return departmentRepository.findById(id).get();
+	}
+	
 	public void delete(String departmentCode) {
 		departmentRepository.deleteById(departmentRepository.findByDepartmentCode(departmentCode).getId());
 	}
@@ -33,4 +54,16 @@ public class DepartmentService {
 	public List<Department> getAll(){
 		return (List<Department>) departmentRepository.findAll();
 	}
+	
+	public boolean isExist(Integer id) {
+		return departmentRepository.existsById(id);
+	}
+	
+	private String[] getNullPropertyNames(Department department) {
+		   final BeanWrapper wrappedSource = new BeanWrapperImpl(department);
+		   return Stream.of(wrappedSource.getPropertyDescriptors())
+		            .map(FeatureDescriptor::getName)
+		            .filter(propertyName -> wrappedSource.getPropertyValue(propertyName) == null)
+		            .toArray(String[]::new);
+		}
 }
